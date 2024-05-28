2b2e9ccbe990637f89b4a07e1f5c28c3e65332c9
LaurBzm <165313394+LaurBzm@users.noreply.github.com>
2024-05-28 13:56:48 +0300

    commit first

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,7 @@
-package org.example;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-
+import java.util.StringJoiner;
 
 
 public class Main {
@@ -13,3 +11,47 @@ public class Main {
         System.out.println(answer);
         }
     }
+import org.apache.commons.lang.StringUtils;
+...
+        ...
+StringJoiner joiner = new StringJoiner("','", "'", "'");
+        joiner.
+
+void add("eu1") {
+
+}
+        joiner.
+
+void add("eu2") {
+
+}
+        joiner.
+
+void add("eu3") {
+
+}
+        joiner.
+
+void add("eu4") {
+
+}
+        System.out.
+
+<joiner> void println(joiner) {
+
+}
+
+private String StringUtils;
+private CharSequence nume;
+String join = "'" + StringUtils.join("','", nume) + "'";
+
+public void main() {
+    String quoted = "author=\"Laur Lumezeanu\"";
+    String unquoted;
+    if(quoted.indexOf("\"") == 0 && quoted.lastIndexOf("\"")==quoted.length()-1){
+        unquoted = quoted.substring(1, quoted.length()-1);
+    }else{
+        unquoted = quoted;
+    }
+}
+
